af755aa0e43d8a3d6d814e0ea273b0163bd94b91
Srishti Sharma <srishtishar@gmail.com>
2021-09-26 00:22:02 +0530

    updated app.js

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ mongoose
      logger.error('error connection to MongoDB: ', error.message)
    })
 
-app.use(express.static(path.join(__dirname, 'build')))
-//app.use(express.static('build'))
+//app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.urlencoded)
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
-app.get('*', function (req, res) {
+/*app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+})*/
 
 module.exports = app
