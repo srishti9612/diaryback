6f23b9e58f3e0fa2fcb69e88c2d0e3d055774bf2
Srishti Sharma <srishtishar@gmail.com>
2021-09-25 23:58:18 +0530

    Changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const path = require('path')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -19,9 +20,9 @@ mongoose
      logger.error('error connection to MongoDB: ', error.message)
    })
 
-
+app.use(express.static(path.join(__dirname, 'build')))
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.urlencoded)
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/users', usersRouter)
